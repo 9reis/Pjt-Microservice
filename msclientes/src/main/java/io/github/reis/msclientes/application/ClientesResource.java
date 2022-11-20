6be5cdf780442ce9988ab0a58f7e0716dd0308aa
Lucas Reis <lucasreis_9@hotmail.com>
2022-11-20 14:33:59 -0300

    Concluindo e testando o Microservice de clientes

--- msclientes/src/main/java/io/github/reis/msclientes/application/ClientesResource.java
+++ msclientes/src/main/java/io/github/reis/msclientes/application/ClientesResource.java
@@ -1,17 +1,54 @@
 package io.github.reis.msclientes.application;
 
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import io.github.reis.msclientes.application.representation.ClienteSaveRequest;
+import io.github.reis.msclientes.domains.Cliente;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/clientes")
+@RequiredArgsConstructor
 public class ClientesResource {
 	
-	
+	private final ClienteService service;
 	
 	@GetMapping
 	public String status() {
 		return "<h1>MsClientes Ok</h1>";
 	}
+	
+	
+	@PostMapping
+	public ResponseEntity save(@RequestBody ClienteSaveRequest request) {
+		Cliente cliente = request.toModel();
+		service.save(cliente);
+		
+		URI headerLocation = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.query("cpf={cpf}")
+				.buildAndExpand(cliente.getCpf())
+				.toUri();
+		return ResponseEntity.created(headerLocation).build();
+	}
+	
+	@GetMapping(params = "cpf")
+	public ResponseEntity dadosCliente(@RequestParam("cpf") String cpf){
+		var cliente = service.getByCPF(cpf);
+		
+		if(cliente.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(cliente);
+	}
+	
 }
