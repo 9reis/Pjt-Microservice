8adcd41b1b5d676bb5eb44065aed1628253835b3
Lucas Reis <lucasreis_9@hotmail.com>
2022-11-21 12:51:12 -0300

    Comunicação sincrona com o OpenFeign

--- msavaliadorcredito/src/main/java/io/github/reis/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/reis/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,16 +1,28 @@
 package io.github.reis.msavaliadorcredito.application;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import io.github.reis.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.reis.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.reis.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
+	private final ClienteResourceClient clientesClient; 
 	
 	public SituacaoCliente obterSituacaoCliente(String cpf) {
+		
+		ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+		
+		return SituacaoCliente
+				.builder()
+				.cliente(dadosClienteResponse.getBody())
+				.build();
+		
 		
 	}
 }
